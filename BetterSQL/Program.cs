e67bbfa9469ea9cdb06ca788ed74c9c1cf46d488
Endergamer1 <janfocke12@outlook.de>
2022-03-28 15:36:05 +0200

    Add Variables for List

--- BetterSQL/Program.cs
+++ BetterSQL/Program.cs
@@ -2,6 +2,17 @@
 
 namespace BetterSQL
 {
+    public class Definition
+    {
+        public string State { get; set; }
+        public int Orders { get; set; }
+        public int RowValue { get; set; }
+        public int FirstValue1 { get; set; }
+        public int FirstValue2 { get; set; }
+
+        public Definition
+
+    }
     class Program
     {
         static void Main(string[] args)
